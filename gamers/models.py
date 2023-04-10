5775d6f62c12941206a49bbc2c49b9a118387653
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-04-10 12:15:51 +0100

    Setup app view, urls, and migrations

--- gamers/models.py
+++ gamers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
--- gamers/views.py
+++ gamers/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def gamers(request):
+    temp = loader.get_template('players.html')
+    return HttpResponse(temp.render())
